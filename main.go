b9c6e73e19df9218e5d71f4b3700802d4600d3fe
Hui Yu <ityuhui@gmail.com>
2019-04-09 14:34:04 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func api1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("html/")))
+	http.HandleFunc("/api1", api1)
 	http.ListenAndServe(":8080", nil)
 }
